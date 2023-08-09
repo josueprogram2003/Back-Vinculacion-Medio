cb1aa9235027ffdf4de86d6fed3b06d8bc8d2c6f
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2023-08-09 15:55:08 -0500

    servicio delete

--- src/controllers/proyecto.controller.js
+++ src/controllers/proyecto.controller.js
@@ -12,3 +12,13 @@ export const getProyectoAll = async(req,res)=>{
     }
   );
 }
+
+export const deleteProject = async (req,res,id)=>{
+  pool.query("delete from PROYECTO WHERE id_proyecto = ? ",[id],function (err,result) {
+    try {
+      return res.status(200).json(result)
+    } catch (error) {
+      return res.status(500).json("Error al listar",error);
+    }
+  })
+}
--- src/routes/proyecto.routes.js
+++ src/routes/proyecto.routes.js
@@ -2,4 +2,8 @@ import { Router } from "express";
 import * as proyecto from "../controllers/proyecto.controller";
 const router = Router();
 router.get("/all", proyecto.getProyectoAll);
+router.delete("/:id", async(req,res)=>{
+  const {id} = req.params;
+  proyecto.deleteProject(req,res,id); 
+});
 export default router;
